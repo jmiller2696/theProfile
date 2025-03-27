2ab670736cb7ca5aee47f1295cc672d4eec12af5
jmiller2696 <143019448+jmiller2696@users.noreply.github.com>
2025-03-27 16:48:35 -0400

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,8 @@
+const profile = ()=> {
+    $("#name").text  ("Joseph Miller");
+    $("#major").text ("CIT");
+    $("#year").text  ("3");
+    $("h4").text  ("USA");
+}
+
+$(document).ready(profile);
